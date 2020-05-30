78e76d71c8fffde67bd2826aac19c1b2ece43537
Rūdolfs Petrovskis <rudolfs93@gmail.com>
2020-05-30 14:34:09 +0300

    Lekcija 25 JS. sakums

---
+++ Lekcijas/Lekcija-25-JS-sakums/test.js
@@ -0,0 +1,25 @@
+var mainigais = 14,
+    variable = 12;
+
+var teksts = "Hello World!";
+
+var izteiksme = mainigais + variable;
+
+//document.write(izteiksme)
+
+//alert(teksts)
+
+//console.log(teksts)
+
+var contact = variable + " Man ir "+ mainigais +" āboli ";
+
+mainigais = 18;
+
+//document.write(mainigais)
+
+if(mainigais == 14){
+  document.write("Ir vienāds")    
+}else{
+  document.write("Nav vienāds")
+}
+
